487ca6f83686c0d56d3fa92afc9ef412438a2ace
umerovzafar <umerov.99@gmail.com>
2022-08-30 16:27:13 +0500

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
+/* swiper slider*/
+
 const slider = document.querySelector(".slider");
+const reviewSlider = document.querySelector(".reviews__slider");
+
 mySwiper = new Swiper(slider, {
     slidesPerView: 3,
     spaceBetween: 25,
@@ -11,5 +15,19 @@ mySwiper = new Swiper(slider, {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+      },
+  });
+mySwiper = new Swiper(reviewSlider, {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    pagination: {
+      el: ".reviews__dots",
+      clickable: true,
+    },
+    slideClass: "reviews__slide",
+    navigation: {
+        nextEl: ".reviews-btn-next",
+        prevEl: ".reviews-btn-prev",
       },
   });
